47f2be09da42aa48eee3d64c1f2f63ee7cf84767
zhang <zhang@gr.cn>
2022-08-27 23:04:33 +0800

    second commit

---
+++ NewClass.java
@@ -0,0 +1,10 @@
+/**
+ * @author:zhang
+ * @date 2022/8/27 - 22:58
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("nono");
+    }
+}

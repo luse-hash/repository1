f690bb5b33044198e1577394e0052740fe74da7b
zhang <zhang@gr.cn>
2022-08-27 23:07:04 +0800

    third commit

--- NewClass.java
+++ NewClass.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @author:zhang
  * @date 2022/8/27 - 22:58
@@ -6,5 +8,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("nono");
+        ArrayList<String> Strings = new ArrayList<>();
     }
 }
